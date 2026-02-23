46edc8f3a5cfa9d8ac1c5e74b7f62d2e8eb14922
Foma Medvedev <tolikslonov5@gmail.com>
2026-02-23 15:10:51 +0500

    реализовал параллельного клиента

--- homework 2/ClusterClient/Clients/ParallelClusterClient.cs
+++ homework 2/ClusterClient/Clients/ParallelClusterClient.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using log4net;
 
@@ -13,9 +15,32 @@ namespace ClusterClient.Clients
         {
         }
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
-            throw new NotImplementedException();
+            var tasks = new List<Task<string>>();
+            for (var i = 0; i < ReplicaAddresses.Length; ++i)
+            {
+                var request = CreateRequest(ReplicaAddresses[i] + "?query=" + query);
+                tasks.Add(ProcessRequestAsync(request));
+            }
+
+            var timeoutTask = Task.Delay(timeout).ContinueWith<string>(_ => throw new TimeoutException());
+
+            while (tasks.Count > 0)
+            {
+                var completedTask = await Task.WhenAny(tasks.Cast<Task>().Concat(new[] {timeoutTask}));
+                if (completedTask == timeoutTask)
+                    throw new TimeoutException();
+                try
+                {
+                    return await (Task<string>)completedTask;
+                }
+                catch
+                {
+                    tasks.Remove((Task<string>)completedTask);
+                }
+            }
+            throw new TimeoutException();
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(ParallelClusterClient));
